d2f15f1e4a30daa06213b7095bed8496918c47d7
Arman <armannqvi.tms@gmail.com>
2026-08-25 16:51:59 +0500

    Fix syntax error

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -125,7 +125,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 # Email
